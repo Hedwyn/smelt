829026c3bd221e013617286bbd638235b9ecf6b5
Hedwyn <bpestourie@gmail.com>
2025-06-12 23:32:38 +0200

    nuitka compile: add include-modules and include-packages options, return bin path

--- src/smelt/nuitkaify.py
+++ src/smelt/nuitkaify.py
@@ -16,9 +16,10 @@ This might be changed later.
 from __future__ import annotations
 
 import logging
+import os
 import subprocess
 import sys
-from typing import Final, Literal
+from typing import Final, Iterable, Literal
 
 _logger = logging.getLogger(__name__)
 
@@ -28,14 +29,22 @@ type Stdout = Literal["stdout", "logger"]
 
 
 def compile_with_nuitka(
-    path: str, no_follow_imports: bool = False, stdout: Stdout | None = None
-) -> None:
+    path: str,
+    no_follow_imports: bool = False,
+    stdout: Stdout | None = None,
+    include_modules: Iterable[str] | None = None,
+    include_packages: Iterable[str] | None = None,
+) -> str:
     """
     Compiles the module given by `path`.
     Follows imports by default, but can be disabled with `no_follow_imports`.
     """
     try:
         import nuitka
+
+        # not using the import - just checking if it is available
+        # as following logic would fail otherwise
+        _ = nuitka
     except ImportError:
         raise ImportError(
             "Nuitka is not installed. Please install this package with nuitka extra: `pip install smelt[nuitka]`."
@@ -45,6 +54,16 @@ def compile_with_nuitka(
         cmd.append("--follow-imports")
     cmd.append("--onefile")
     cmd.append(path)
+
+    # handling special flags
+    if include_modules:
+        for mod in include_modules:
+            cmd.append(f"--include-module={mod}")
+
+    if include_packages:
+        for package in include_packages:
+            cmd.append(f"--include-package={package}")
+
     _logger.debug("Running %s", " ".join(cmd))
 
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -61,3 +80,7 @@ def compile_with_nuitka(
             print(decoded_line)
 
     _logger.info("[Nuitka]: %d", proc.returncode)
+    expected_extension = ".exe" if sys.platform == "Windows" else ".bin"
+    bin_path = os.path.basename(path).replace(".py", expected_extension)
+    assert os.path.exists(bin_path)
+    return bin_path
