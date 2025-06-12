80020e5bbf781b36e494ba0bf8acba2fad24ceaf
Hedwyn <bpestourie@gmail.com>
2025-06-12 17:47:00 +0200

    fix example minimal project, add script entrypoints to it

--- examples/minimal/src/minimal/cli.py
+++ examples/minimal/src/minimal/cli.py
@@ -1,12 +1,25 @@
+import time
+
 import click
 
-from minimal.hello import hello
 from minimal.fib import fib
 
-@click.command()
-@click.argumet("name", type=int)
-def main(n: int):
-    hello()
-    print("Computing fibonacci of ", n)
-    print(fib(n))
 
+@click.group()
+def minimal(): ...
+
+
+@minimal.command()
+def say_hello():
+    from minimal.hello import hello
+
+    hello()
+
+
+@minimal.command()
+@click.argument("n", type=int)
+def compute_fib(n: int):
+    start_time = time.time()
+    click.echo(f"Computing fibonacci of {n}")
+    click.echo(fib(n))
+    click.echo(f"Computation took {time.time() - start_time}")
