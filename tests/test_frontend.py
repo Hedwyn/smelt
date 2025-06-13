5e9733423b0ccb5dde56ffe8183ecc807fe5d10b
Hedwyn <bpestourie@gmail.com>
2025-06-13 09:24:50 +0200

    add tests for config parsing in frontend

---
+++ tests/test_frontend.py
@@ -0,0 +1,38 @@
+"""
+Test suite for smelt frontend.
+
+@date: 13.06.2025
+@author: Baptiste Pestourie
+"""
+
+from __future__ import annotations
+
+import pytest
+import tomllib
+
+from smelt.frontend import TomlData, parse_config_from_pyproject
+
+SAMPLE_CONFIG: str = """\
+[tool.smelt]
+mypyc = ["minimal.fib"]
+c_extensions=["minimal.hello"]
+entrypoint="minimal.cli"
+"""
+
+
+@pytest.fixture
+def toml_data() -> TomlData:
+    """
+    Example TOML data as it would be extraced from pyproject.toml
+    """
+    return tomllib.loads(SAMPLE_CONFIG)
+
+
+def test_parse(toml_data: TomlData) -> None:
+    """
+    Verifies that the pyproject parse extracts Smelt config properly
+    """
+    config = parse_config_from_pyproject(toml_data)
+    assert config.entrypoint == "minimal.cli"
+    assert config.c_extensions == ["minimal.hello"]
+    assert config.mypyc == ["minimal.fib"]
