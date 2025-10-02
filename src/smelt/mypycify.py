a9fc914e6a787878aec8f26f3566c15549d78a6e
Hedwyn <bpestourie@gmail.com>
2025-10-02 15:22:58 +0200

    allow module discovery strategy to be customized in mypycify

--- src/smelt/mypycify.py
+++ src/smelt/mypycify.py
@@ -6,7 +6,7 @@ Mypyc wrapper, resolves some logic specific to mypyc extensions.
 """
 
 from __future__ import annotations
-
+from typing import assert_never
 import sysconfig
 from dataclasses import dataclass
 from pathlib import Path
@@ -14,7 +14,13 @@ from pathlib import Path
 from mypyc.build import mypycify
 from setuptools._distutils.extension import Extension
 
-from smelt.utils import get_extension_suffix, import_shadowed_module
+from smelt.utils import (
+    get_extension_suffix,
+    import_shadowed_module,
+    ModpathType,
+    toggle_mod_path,
+    find_module_in_layout,
+)
 
 
 @dataclass
@@ -69,14 +75,32 @@ class MypycExtension:
 def mypycify_module(
     import_path: str,
     extpath: str,
+    strategy: ModpathType = ModpathType.IMPORT,
 ) -> MypycExtension:
-    with import_shadowed_module(import_path) as mod:
-        # TODO: seems that mypy detects the package and names the module package.mod
-        # automatically ?
-        assert mod.__file__ is not None
-        runtime, module_ext = mypycify([extpath], include_runtime_files=True)
-        mod_folder = Path(mod.__file__).parent
-        ext_name = module_ext.name.split(".")[-1]
+    match strategy:
+        case ModpathType.IMPORT:
+            curated_import_path = toggle_mod_path(import_path, ModpathType.IMPORT)
+            with import_shadowed_module(curated_import_path) as mod:
+                # TODO: seems that mypy detects the package and names the module package.mod
+                # automatically ?
+                assert mod.__file__ is not None
+                runtime, module_ext = mypycify([extpath], include_runtime_files=True)
+                mod_folder = Path(mod.__file__).parent
+                ext_name = module_ext.name.split(".")[-1]
+
+        case ModpathType.FS:
+            curated_import_path = find_module_in_layout(
+                toggle_mod_path(import_path, ModpathType.FS)
+            )
+            runtime, module_ext = mypycify(
+                [curated_import_path], include_runtime_files=True
+            )
+            mod_folder = Path(curated_import_path).parent
+
+        case _ as unreachable:
+            assert_never(unreachable)
+
+    ext_name = module_ext.name.split(".")[-1]
 
     return MypycExtension(
         import_path=import_path,
