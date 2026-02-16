6710df2a34e9a0215ae771f00fe4c12b51c7f707
Baptiste Pestourie <bpestourie@gmail.com>
2026-02-16 14:32:50 +0100

    breaking change: modify cython config and update hatchdemo example accordingly

--- examples/hatchdemo/src/hatchdemo/cli.py
+++ examples/hatchdemo/src/hatchdemo/cli.py
@@ -4,6 +4,7 @@ import click
 
 from hatchdemo.fib import fib as fib_mypyc
 from hatchdemo.fib_purepython import fib as fib_purepython
+from hatchdemo.fib_cython import fib as fib_cython
 
 
 @click.group()
@@ -21,16 +22,22 @@ def say_hello():
 @click.argument("n", type=int)
 def compute_fib(n: int):
     start_time = time.time()
-
     click.echo(f"Computing fibonacci of {n} [pure python]")
     purepy_result = fib_purepython(n)
-    click.echo(f"Computation took {time.time() - start_time}")
+    click.echo(f"Computation took {time.time() - start_time} s")
 
+    start_time = time.time()
     click.echo(f"Computing fibonacci of {n} [mypyc]")
     mypyc_result = fib_mypyc(n)
-    click.echo(f"Computation took {time.time() - start_time}")
+    click.echo(f"Computation took {time.time() - start_time} s")
+
+    start_time = time.time()
+    click.echo(f"Computing fibonacci of {n} [cython]")
+    cython_result = fib_cython(n)
+    click.echo(f"Computation took {time.time() - start_time} s")
 
     assert purepy_result == mypyc_result
+    assert purepy_result == cython_result
     click.echo(f"Result is {purepy_result}, by the way")
 
 
